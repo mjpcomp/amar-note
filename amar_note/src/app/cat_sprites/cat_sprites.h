f1387f9f91aa8ac94f7605fbf847841cec869556
mjpcomp <7637082+mjpcomp@users.noreply.github.com>
2026-07-30 00:02:50 -0700

    Add cat_sprites/cat_sprites.h umbrella header (drop-in from defcon1702/pala-nekogotchi)

---
+++ amar_note/src/app/cat_sprites/cat_sprites.h
@@ -0,0 +1,13 @@
+#pragma once
+// Aggregate header for all cat sprites (MVP).
+#define CAT_W 150
+#define CAT_H 150
+
+#include "cat_content.h"
+#include "cat_happy.h"
+#include "cat_hungry.h"
+#include "cat_sleep.h"
+#include "cat_sad.h"
+#include "cat_eat.h"
+#include "cat_play.h"
+#include "cat_purr.h"
